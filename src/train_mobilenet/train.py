9e10a45c2f6fff85cfbd824d930d6eabdcf98fa1
AndMit <andreas6899@gmail.com>
2022-07-03 17:15:30 +0300

    name change

--- src/train_mobilenet/train.py
+++ src/train_mobilenet/train.py
@@ -14,27 +14,27 @@ print("The following GPU devices are available: %s" % tf.test.gpu_device_name())
 model = mobilenet_segnet(n_classes=2 ,  input_height=224, input_width=224  )
 
 
-model.summary()
+# model.summary()
 
-model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer='adadelta', loss='categorical_crossentropy', metrics=['accuracy'])
 
-model.train(
-    train_images =  "ResizedFr/",
-    train_annotations = "CMasks/",
-    checkpoints_path = "mobilenet_checkpoints" , epochs=50,
-    batch_size=10,
-    verify_dataset=True,
-    validate=True,
-    val_images="TestFr",
-    val_annotations="TestM",
-    val_batch_size=10,
-    auto_resume_checkpoint=True,
-    load_weights=None,
-    steps_per_epoch=512,
-    val_steps_per_epoch=512,
-    gen_use_multiprocessing=False,
-    ignore_zero_class=False,
-    optimizer_name='adadelta',
-    do_augment=False,
-    augmentation_name="aug_all",
-)
+# model.train(
+#     train_images =  "ResizedFr/",
+#     train_annotations = "CMasks/",
+#     checkpoints_path = "mobilenet_checkpoints" , epochs=50,
+#     batch_size=10,
+#     verify_dataset=True,
+#     validate=True,
+#     val_images="TestFr",
+#     val_annotations="TestM",
+#     val_batch_size=10,
+#     auto_resume_checkpoint=True,
+#     load_weights=None,
+#     steps_per_epoch=512,
+#     val_steps_per_epoch=512,
+#     gen_use_multiprocessing=False,
+#     ignore_zero_class=False,
+#     optimizer_name='adadelta',
+#     do_augment=False,
+#     augmentation_name="aug_all",
+# )
